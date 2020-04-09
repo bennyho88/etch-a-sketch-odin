33f5d8ed1d8e9dd5f38b63240f54a6701b1a54c3
Benny Ho <benny.ho2530@gmail.com>
2020-04-09 15:32:35 +0200

    added button event listeners

--- app.js
+++ app.js
@@ -2,36 +2,69 @@
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
-const container = document.querySelector('.grid-container');
-const buttons = document.querySelectorAll('.box');
+    const container = document.querySelector('.grid-container');
 
+    let rows = 16;
+    let columns = 16;
 
-let rows = 16;
-let columns= 16;
+    let total = rows * columns;
+    console.log(total);
 
-let total = rows * columns;
-console.log(total);
+    for (var i = 1; i <= total; i++) {
 
-for (var i = 1; i <= total; i++ ) {
+        const div = document.createElement('div');
+        div.classList.add('box');
 
-    const div = document.createElement('div');
-    div.classList.add('box');
+        container.appendChild(div);
 
-    container.appendChild(div);
-}
+    }
 
-buttons.forEach( button => {
+    const gridContainer = document.querySelectorAll('.box');
 
-    console.log(button);
+    gridContainer.forEach(grid => {
 
-    /*
-    button.addEventListener('onmouseenter', function() {
+        grid.addEventListener('mouseover', function () {
 
-        console.log('hello');
+            grid.classList.add('color');
+        })
     })
-    */
-})
+
+    const buttons = document.querySelectorAll('.btn');
+
+    buttons.forEach(button => {
+
+        button.addEventListener('click', function () {
+
+
+            if (button.classList.contains('clear')) {
+
+                const gridContainer = document.querySelectorAll('.box');
+
+                gridContainer.forEach(grid => {
+
+                    grid.classList.remove('color');
+                })
+
+            } else if(button.classList.contains('erase')) {
+
+                const gridContainer = document.querySelectorAll('.box');
+
+                gridContainer.forEach( grid => {
+
+                    grid.addEventListener('mouseover', function() {
+
+                        grid.classList.toggle('color');
+                    })
+                })
+            }
+
+
+
+
+        })
+    })
 
 })
+
